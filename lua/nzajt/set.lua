e92c5c19105cabe51ab2e0b116618bcf5d06da0c
Nathan Daily <ndaily@netmediagroup.com>
2022-12-19 11:18:51 -0700

    move signcolumn to 100

--- lua/nzajt/set.lua
+++ lua/nzajt/set.lua
@@ -7,9 +7,9 @@ vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -32,4 +32,4 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
