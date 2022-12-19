ae212780f17b6266af0bc1501e7915e2ed95bf13
Nathan Daily <ndaily@netmediagroup.com>
2022-12-18 19:45:00 -0700

    forket and trying to get it wokring on my nvim

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-### ThePrimeagen's init.lua
+### Nzajt's init.lua
+I stole this from https://github.com/ThePrimeagen/init.lua/
+
 Prerequisite: install [ripgrep](https://github.com/BurntSushi/ripgrep).
 
 [The full video of me setting up this repo](https://www.youtube.com/watch?v=w7i4amO_zaE)
--- lua/nzajt/init.lua
+++ lua/nzajt/init.lua
@@ -1,8 +1,8 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
+require("nzajt.set")
+require("nzajt.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local NzajtGroup = augroup('Nzajt', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -23,7 +23,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = NzajtGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
