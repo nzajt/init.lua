761d18a598ddd116ebd44586db8fa2987865148e
Nathan Daily <ndaily@netmediagroup.com>
2023-01-13 10:00:06 -0700

    another try at all buffers

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,8 +1,12 @@
 require('cmp').setup({
   sources = {
     { name = 'path' },
-    { name = 'nvim_lsp', keyword_length = 3 },
-    { name = 'buffer', keyword_length = 3 },
-    { name = 'luasnip', keyword_length = 2 },
+    { name = 'nvim_lsp', keyword_length = 2 },
+    { name = 'buffer',
+      keyword_length = 2,
+      get_bufnrs = function()
+        return vim.api.nvim_list_bufs()
+      end
+    },
   },
 })
