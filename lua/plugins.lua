8bdaee8ee2f0f144a679d28c9b201539c035e3f8
Nathan Daily <ndaily@netmediagroup.com>
2023-01-17 10:18:48 -0700

    update nvim comment

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,10 +15,6 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
 
   use "terrortylor/nvim-comment"
-  require('nvim_comment').setup(
-    { line_mapping = "<leader>lc", operator_mapping = "<leader>cc", comment_chunk_text_object = "ic" }
-  )
-
   use {
     'VonHeikemen/lsp-zero.nvim',
     requires = {
