8d0515b8b0585110a8865505c698b57ae337c537
Nathan Daily <ndaily@netmediagroup.com>
2023-10-23 13:59:36 -0600

    cleaning up issue with <leader>f

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -14,7 +14,7 @@ null_ls.setup({
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
+      vim.keymap.set("n", "<Leader>gf", function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
       end, { buffer = bufnr, desc = "[lsp] format" })
 
@@ -31,9 +31,29 @@ null_ls.setup({
     end
 
     if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
+      vim.keymap.set("x", "<Leader>gf", function()
         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
       end, { buffer = bufnr, desc = "[lsp] format" })
     end
   end,
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
 })
